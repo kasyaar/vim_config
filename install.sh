a4b54e44b531a488e57f5f13aee23da1932f340e
Dmitry Kasimtsev <kasyaar@gmail.com>
2024-10-12 20:49:48 +0300

    added old .vim dir backup step to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 echo "Installing vim_config"
 if command -v git > /dev/null 2>&1; then
+    echo "Backing up old .vim directory"
+    mv ~/.vim ~/.vim.$(date +%Y%m%d-%H%M%S) > /dev/null 2>&1
     echo "Getting package from https://github.com/kasyaar/vim_config.git..."
     git clone https://github.com/kasyaar/vim_config.git ~/.vim > /dev/null 2>&1
     ln -s ~/.vim/.vimrc ~/ > /dev/null 2>&1
