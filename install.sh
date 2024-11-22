38ae257fbbf9312718582d014a92f2d10bb52512
Dmitry Kasimtsev <kasyaar@gmail.com>
2024-11-22 20:37:30 +0200

    feat: improve installation script
    The installation script now checks for required dependencies (vim, git, pipx, node, npm) before proceeding.  It also provides a more informative error message if any of the dependencies are missing.  Furthermore, the installation of plugins and bandit is streamlined.

--- install.sh
+++ install.sh
@@ -1,45 +1,31 @@
 #!/usr/bin/env sh
-check_editor() {
-    local editor="$1"
-    if command -v "$editor" > /dev/null 2>&1; then
-        return 0
-    else
-        return 1
-    fi
-}
-echo "Installing vim_config"
-if command -v git > /dev/null 2>&1; then
-    echo "Backing up old .vim directory"
-    mv ~/.vim ~/.vim.$(date +%Y%m%d-%H%M%S) > /dev/null 2>&1
-    echo "Getting package from https://github.com/kasyaar/vim_config.git..."
-    git clone https://github.com/kasyaar/vim_config.git ~/.vim > /dev/null 2>&1
-    ln -sf ~/.vim/.vimrc ~/ > /dev/null 2>&1
-    ln -sf ~/.vim/.gvimrc ~/ > /dev/null 2>&1
-else
-    echo "git is not installed. Please install and run again."
-fi
+missing=""
+commands="vim git pipx node npm"
+set -- $commands
+for command; do
+    command -v "$command" 2>&1 /dev/null || { echo "Error: $command not found"; missing="$missing $command"; }
+done
 
-if check_editor vim || check_editor nvim; then
-    EDITOR=$(command -v vim || command -v nvim)
-    if check_editor nvim; then
-        NVIM_CONFIG_PATH=$HOME/.config/nvim
-        mkdir -p $NVIM_CONFIG_PATH > /dev/null 2>&1
-        cp $HOME/.vim/init.vim.example $NVIM_CONFIG_PATH/init.vim
-    fi
-    echo "Installing vim plugins($EDITOR +PlugInstall +qall)..."
-    #$EDITOR +PlugInstall +qall > /dev/null 2>&1
-    $EDITOR +PlugInstall +qall
+if [ -z "$missing" ]; then
+    echo "Installing vim_config"
 else
-    echo "vim/neovim is not installed. Please install and run again."
+    echo "The following apps are missing: $missing"
+    exit 1
 fi
-if command -v pipx > /dev/null 2>&1; then
-    #echo "Installing bandit and python-lsp-server"
-    echo "Installing bandit"
-    pipx install bandit > /dev/null 2>&1
-    #pipx install "python-lsp-server[all]" > /dev/null 2>&1
-    echo "Installation finished."
-    echo "Don't forget to install nodejs and npm."
-    echo "It's required for code completion and other features."
-else
-    echo "pipx need to be installed"
-fi
+echo "Backing up old .vim directory"
+mv ~/.vim ~/.vim.$(date +%Y%m%d-%H%M%S) > /dev/null 2>&1
+echo "Getting package from https://github.com/kasyaar/vim_config.git..."
+git clone https://github.com/kasyaar/vim_config.git ~/.vim > /dev/null 2>&1
+ln -sf ~/.vim/.vimrc ~/ > /dev/null 2>&1
+ln -sf ~/.vim/.gvimrc ~/ > /dev/null 2>&1
+
+NVIM_CONFIG_PATH=$HOME/.config/nvim
+mkdir -p $NVIM_CONFIG_PATH > /dev/null 2>&1
+cp $HOME/.vim/init.vim.example $NVIM_CONFIG_PATH/init.vim
+echo "Installing vim plugins(vim +PlugInstall +qall)..."
+vim +PlugInstall +qall > /dev/null 2>&1
+
+echo "Installing bandit"
+pipx install bandit > /dev/null 2>&1
+#pipx install "python-lsp-server[all]" > /dev/null 2>&1
+echo "Installation finished."
